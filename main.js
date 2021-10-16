b8787b37d4a6712842f0c5b26ea6378ee8920aae
Jiya-Kumari <89457678+Jiya-Kumari@users.noreply.github.com>
2021-10-16 21:47:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.image.fromURL("BirthdayImage.jpg",function(img){
+		birthday_object=img;
+		birthday_object.ScaleToWidth(50);
+		birthday_object.ScaleToHeight(50);
+		birthday_object.set({
+			top:birthday_y,
+			left:birthday_x
+		});
+		canvas.add(BirthdayImage.jpg);
+}
+
+         function playsound({
+			x.play("bdayaudio.mp3");
+		})
+block_image_object.set({
+	top:0,
+	left:0
+canvas.add("block_image_object");
+})
